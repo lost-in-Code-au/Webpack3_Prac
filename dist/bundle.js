4d43393017d6eb5256a4c37f9b8993ecfc2632ca
lost-in-Code-au <reece.c.jones@gmail.com>
2018-01-23 16:29:29 +0000

    correction of stage-0

--- dist/bundle.js
+++ dist/bundle.js
@@ -65,14 +65,9 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-
-
-var arr = ['JavaScript', 'Node', 'Webpack', 'React', 'Angular', 'Ember'];
-
-console.log(arr);
+throw new Error("Module build failed: Error: Couldn't find preset \"stage-0\" relative to directory \"/Users/lost-in-code/Development_Projects/Lynda_dev/Webpack3/Webpack3_Prac\"\n    at /Users/lost-in-code/Development_Projects/Lynda_dev/Webpack3/Webpack3_Prac/node_modules/babel-core/lib/transformation/file/options/option-manager.js:293:19\n    at Array.map (native)\n    at OptionManager.resolvePresets (/Users/lost-in-code/Development_Projects/Lynda_dev/Webpack3/Webpack3_Prac/node_modules/babel-core/lib/transformation/file/options/option-manager.js:275:20)\n    at OptionManager.mergePresets (/Users/lost-in-code/Development_Projects/Lynda_dev/Webpack3/Webpack3_Prac/node_modules/babel-core/lib/transformation/file/options/option-manager.js:264:10)\n    at OptionManager.mergeOptions (/Users/lost-in-code/Development_Projects/Lynda_dev/Webpack3/Webpack3_Prac/node_modules/babel-core/lib/transformation/file/options/option-manager.js:249:14)\n    at OptionManager.init (/Users/lost-in-code/Development_Projects/Lynda_dev/Webpack3/Webpack3_Prac/node_modules/babel-core/lib/transformation/file/options/option-manager.js:368:12)\n    at File.initOptions (/Users/lost-in-code/Development_Projects/Lynda_dev/Webpack3/Webpack3_Prac/node_modules/babel-core/lib/transformation/file/index.js:212:65)\n    at new File (/Users/lost-in-code/Development_Projects/Lynda_dev/Webpack3/Webpack3_Prac/node_modules/babel-core/lib/transformation/file/index.js:135:24)\n    at Pipeline.transform (/Users/lost-in-code/Development_Projects/Lynda_dev/Webpack3/Webpack3_Prac/node_modules/babel-core/lib/transformation/pipeline.js:46:16)\n    at transpile (/Users/lost-in-code/Development_Projects/Lynda_dev/Webpack3/Webpack3_Prac/node_modules/babel-loader/lib/index.js:50:20)\n    at Object.module.exports (/Users/lost-in-code/Development_Projects/Lynda_dev/Webpack3/Webpack3_Prac/node_modules/babel-loader/lib/index.js:175:20)");
 
 /***/ })
 /******/ ]);
